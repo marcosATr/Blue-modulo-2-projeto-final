6f2dd43264f4f429d77edf9a62a18b9de6302def
marcosATr <marcos.trannin@gmail.com>
2021-07-07 07:44:16 -0300

    [ADD] JS included

---
+++ static/moveSidebar.js
@@ -0,0 +1,12 @@
+const sidebar = {
+    visibility: false,
+    position: document.querySelector('#sidebar')
+}
+//const sidebar = document.querySelector('#sidebar')
+const menu = document.querySelector('.hiddenNav')
+console.log(sidebar.visibility)
+menu.addEventListener('click', function(){
+    sidebar.visibility =! sidebar.visibility
+    sidebar.visibility ? sidebar.position.classList.add('sidebarNotHidden') : sidebar.position.classList.remove('sidebarNotHidden')
+
+})
